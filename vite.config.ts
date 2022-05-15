703b400f109ac1d295d22dd16c57a7611acbbf1a
Ingus Mat Burleson <ingusmat@gmail.com>
2022-05-15 00:22:52 -0400

    deploy configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default {
-  base: "https:ingusmat.github.io/in3d/",
-  plugins: [react()]
-}
+export default defineConfig({
+  plugins: [react()],
+  base: './in3d/',
+})
