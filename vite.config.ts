ecc7464ed41048022f55408f189748ef04dbb2c7
Ingus Mat Burleson <ingusmat@gmail.com>
2022-05-16 17:55:36 -0400

    TS Ignore chair file for now

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'http://ingusmat.com/',
+  base: '/in3d/',
+  assetsInclude: ['**/*.glb']
 })
